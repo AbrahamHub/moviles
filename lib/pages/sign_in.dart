92b877ea82bf59e232266c6919d30bbb0fe29394
Fernando Rodriguez <feransic@gmail.com>
2024-09-24 16:57:11 -0600

    Se agrega el login

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -60,8 +60,104 @@ class SignIn extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(height: size.height * 0.04),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  Container(
+                    width: size.width,
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: buttonColor,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 22),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.06),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 2,
+                        width: size.width * 0.25,
+                        color: Colors.black12,
+                      ),
+                      const Text(
+                        "  Or continue with  ",
+                        style: TextStyle(
+                            color: Color(0xff6F6B7A),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      Container(
+                        height: 2,
+                        width: size.width * 0.25,
+                        color: Colors.black12,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: size.height * 0.06),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      socialIcon("assets/images/google.png"),
+                      socialIcon("assets/images/google.png"),
+                      socialIcon("assets/images/google.png"),
+                    ],
+                  ),
+                  SizedBox(height: size.height * 0.07),
+                  Text.rich(
+                    TextSpan(
+                        text: "Not a member? ",
+                        style: TextStyle(
+                          color: textColor2,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                        children: const [
+                          TextSpan(
+                            text: "Register now",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ]),
+                  ),
+                ],
+              ),
+            )
           ],
         )),
+      ),
+    );
+  }
+
+  Container socialIcon(image) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 15,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.white,
+          width: 2,
+        ),
+      ),
+      child: Image.asset(
+        image,
+        height: 35,
       ),
     );
   }
