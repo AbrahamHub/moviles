f7c55743b9e2f660cabf9d5567803dcd274e52ba
latas-dev <thelonelysuric@gmail.com>
2024-10-31 15:23:06 -0600

    .env conf

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:PhotoGuard/pages/spash_screen.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  // acceso a las claves desde la app
+  // ejemplo:
+  // final apiKey = dotenv.env['FIREBASE_API_KEY'];
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
